bfeebc0411127fe1c3a914a880883f59e86c8e52
firman-cn <firmancitranurrahman@gmail.com>
2025-06-21 22:47:04 +0700

    add template

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,19 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <a href="#" class="brand-link">
+    <span class="brand-text font-weight-light">e-Warmad</span>
+  </a>
+
+  <div class="sidebar">
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview">
+        <li class="nav-item">
+          <a href="/admin" class="nav-link">
+            <i class="nav-icon fas fa-tachometer-alt"></i>
+            <p>Dashboard</p>
+          </a>
+        </li>
+        <!-- Tambahkan menu lainnya -->
+      </ul>
+    </nav>
+  </div>
+</aside>
