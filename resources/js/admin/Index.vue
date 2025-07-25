57eaf5a06abc35251a2023f1578861b2b6c29e26
firman-cn <firmancitranurrahman@gmail.com>
2025-07-25 23:09:06 +0700

    tes

---
+++ resources/js/admin/Index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1>Dashboard Admin</h1>
+    <div class="row">
+      <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-info">
+          <div class="inner">
+            <h3>150</h3>
+            <p>Pengguna</p>
+          </div>
+          <div class="icon">
+            <i class="fas fa-users"></i>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-6">
+        <div class="small-box bg-success">
+          <div class="inner">
+            <h3>53<sup style="font-size: 20px">%</sup></h3>
+            <p>Transaksi Sukses</p>
+          </div>
+          <div class="icon">
+            <i class="fas fa-chart-line"></i>
+          </div>
+        </div>
+      </div>
+
+      <!-- Tambahkan lebih banyak box jika perlu -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index"
+}
+</script>
+
+<style scoped>
+.small-box {
+  border-radius: 0.5rem;
+  padding: 1rem;
+  color: #fff;
+}
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,7 @@
 // import './bootstrap';
 import { createApp } from 'vue'
 import App from './components/App.vue'
+import Index from './admin/Index.vue'
 
 createApp(App).mount("#app")
+createApp(Index).mount("#index")
